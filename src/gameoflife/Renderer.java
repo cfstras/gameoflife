13c4d4e888754ea77cdf964c81ff667fd68e6f47
nixnick <nixnick@a70f5666-fd2b-40d0-8f41-e30882f606c2>
2012-01-29 01:26:25 +0000

    some docs for renderer
    git-svn-id: https://svn.q1cc.net/repository/nixnick/gameoflife@503 a70f5666-fd2b-40d0-8f41-e30882f606c2

--- src/gameoflife/Renderer.java
+++ src/gameoflife/Renderer.java
@@ -10,8 +10,21 @@ package gameoflife;
  */
 public interface Renderer {
     
+    
+    /**
+     * Inits the renderer and gives it a field,
+     * just to set the right size parameters and draw an initial image.
+     * 
+     * @param field 
+     */
     public void init(GameField field);
     
+    /**
+     * The drawField method is invoked once for each generation that should be rendered
+     * and passes the GameField containing the new generation along.
+     * Notice that these get overwritten every other generation (since we are using a so-called "double-buffer")
+     * @param field 
+     */
     public void drawField(GameField field);
     
     
