07144f2581d49df76c4c8e55f16c2dc234455df2
nixnick <nixnick@a70f5666-fd2b-40d0-8f41-e30882f606c2>
2012-01-29 01:06:19 +0000

    simple and inefficient GOL ruleset implemented
    git-svn-id: https://svn.q1cc.net/repository/nixnick/gameoflife@500 a70f5666-fd2b-40d0-8f41-e30882f606c2

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -12,7 +12,7 @@ public class GameOfLife {
      * @param currentField the field to read data from.
      * @param generateTo the field where the data will be written
      */
-    public void generation(GameField currentField, GameField generateTo) {
+    public static void generation(GameField currentField, GameField generateTo) {
         if(currentField.width!=generateTo.width || currentField.height!=generateTo.height) {
             //what the holy fuck are you trying to do?
             throw new RuntimeException();
@@ -21,20 +21,57 @@ public class GameOfLife {
         generateTo.lock.writeLock().lock();
         currentField.lock.readLock().lock();
         for(int ix=0;ix<currentField.width;ix++) {
-            //TODO
+            for(int iy=0;iy<currentField.height;iy++) {
+                byte numNeighbours=getNumOfNeighbours(currentField, ix, iy);
+                if(currentField.aliveCells[ix][iy]) {
+                    //i'm alive!
+                    if(numNeighbours<2) {
+                        generateTo.aliveCells[ix][iy]=false; //alive, less than 2 neighbours --> DEATH
+                    } else if(numNeighbours<=3) {
+                        generateTo.aliveCells[ix][iy]=true; // alive, 2/3 neighbours --> lives on
+                    } else {
+                        generateTo.aliveCells[ix][iy]=false; // alive, 4+ neighbours --> overcrowding
+                    }
+                } else {
+                    //i'm dead!
+                    if(numNeighbours==3) {
+                        generateTo.aliveCells[ix][iy]=true; // dead, 3 neighbours -> new life!
+                    } else {
+                        generateTo.aliveCells[ix][iy]=false; // anything else won't do nothing.
+                    }
+                }
+            }
         }
         
-            
         generateTo.lock.writeLock().unlock();
         currentField.lock.readLock().unlock();
     }
     
-    byte getNumOfNeighbors(GameField f,int x,int y) {
-        byte numNeighbors=0;
+    /**
+     * temp value
+     */
+    static byte numNeighbours;
+    static byte getNumOfNeighbours(GameField f,int x,int y) {
+        numNeighbours=0;
+        addNeighbour(f, x-1, y-1);
+        addNeighbour(f, x-1, y);
+        addNeighbour(f, x-1, y+1);
+        addNeighbour(f, x, y-1);
+        addNeighbour(f, x, y+1);
+        addNeighbour(f, x+1, y-1);
+        addNeighbour(f, x+1, y);
+        addNeighbour(f, x+1, y+1);
         
-        
-        
-        return 0;
+        return numNeighbours;
     }
+    
+    private static void addNeighbour(GameField f,int x,int y) {
+        if(x>=0 && x < f.width && y>=0 && y < f.height ) {
+            if(f.aliveCells[x][y]) {
+                numNeighbours++;
+            }
+        }
+    }
+    
     
 }
--- src/gameoflife/Main.java
+++ src/gameoflife/Main.java
@@ -11,8 +11,52 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        GameField life = new GameField(10, 10, "Leben");
-        life.insertRandomData();
-        life.printGameField();
+        //GameField life = new GameField(10, 10, "Leben");
+        //life.insertRandomData();
+        //life.printGameField();
+        mainLoop();
     }
+    
+    public static GameField gliderField() {
+        GameField f=new GameField(10, 10, "glider1");
+        f.aliveCells = new boolean[][] {
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,true ,false,false,false,false,false},
+        {false,false,true ,false,true ,false,false,false,false,false},
+        {false,false,false,true ,true ,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false},
+        {false,false,false,false,false,false,false,false,false,false}
+    };
+        
+        return f;
+    }
+    
+    public static void mainLoop() {
+        GameField current=gliderField();
+        GameField next=new GameField(10,10,"glider2");
+        
+        while(true) {
+            //print
+            System.out.println();System.out.println();System.out.println();System.out.println();
+            current.printGameField();
+            
+            //generation
+            GameOfLife.generation(current, next);
+            //swap buffers
+            GameField temp=current;
+            current=next;
+            next=temp;
+            
+            try {
+                //wait
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {}
+        }
+        
+    }
+    
 }
