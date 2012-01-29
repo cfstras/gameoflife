0a5841fae475aeca3da4dc69aec63e81848c9b2a
morth <morth@a70f5666-fd2b-40d0-8f41-e30882f606c2>
2012-01-29 01:18:50 +0000

    swingui added
    git-svn-id: https://svn.q1cc.net/repository/nixnick/gameoflife@501 a70f5666-fd2b-40d0-8f41-e30882f606c2

--- src/gameoflife/Main.java
+++ src/gameoflife/Main.java
@@ -11,10 +11,17 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        GameField life = new GameField(10, 10, "Leben");
+        life.insertRandomData();
+        SwingGUI gui = new SwingGUI(10,10,"GUI");
+        gui.init(life);
+        gui.drawField(life);
+
         //GameField life = new GameField(10, 10, "Leben");
         //life.insertRandomData();
         //life.printGameField();
-        mainLoop();
+        //mainLoop();
     }
     
     public static GameField gliderField() {
---
+++ src/gameoflife/SwingGUI.java
@@ -0,0 +1,65 @@
+package gameoflife;
+
+import java.awt.*;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import javax.swing.*;
+
+public class SwingGUI extends JPanel implements Renderer {
+    
+    private static final long serialVersionUID = -2294685016438617741L;
+    private static final Random r = new Random();
+    int width, height;
+    String name;
+    GameField field;
+    int datax, datay;
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        field.lock.readLock().lock();
+        if (field != null) {
+            try {
+                datax = 0;
+                datay = 0;
+                for (int y = 12; y < 25 * width - 25; y += 30) {
+                    for (int x = 12; x < 25 * height - 25; x += 30) {
+                        if (field.aliveCells[datax++][datay]) {
+                            g.setColor(Color.BLACK);
+                        } else {
+                            g.setColor(Color.WHITE);
+                        }
+                        g.fillRect(x, y, 25, 25);
+                        g.setColor(Color.WHITE);
+                        g.drawRect(x - 1, y - 1, 25, 25);
+                    }
+                    datay++;
+                }
+            } finally {
+                field.lock.readLock().unlock();
+            }
+        }
+    }
+    
+    public SwingGUI(int width, int height, String name) {
+        this.width = width;
+        this.height = height;
+        this.name = name;
+    }
+    
+    @Override
+    public void init(GameField field) {
+        this.field = field;
+        invalidate();
+        repaint();
+    }
+    
+    @Override
+    public void drawField(GameField field) {
+        JFrame f = new JFrame("GUI: " + name);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(height * 25, width * 25);
+        f.add(new SwingGUI(width, height, name));
+        f.setVisible(true);
+    }
+}
